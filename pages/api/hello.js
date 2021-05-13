7992f737dbb4ef3405b8fa2d1a5fcadd62db79a9
Andrew Flett <andrflett@gmail.com>
2021-05-13 13:25:58 +0100

    WIP

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,11 +2,26 @@ import axios from "axios"
 
 export default async function SendQuestion(req, res) {
 
-  console.log(req)
+  console.log(req.body[0]['Stock Counter'])
 
-  await SendEmail('hi', 'there').then(response =>
-    res.status(200).json({ response })
-  )
+  let message = null
+  let subject = null
+
+  if(req.body[0]['Stock Counter'] == "Sold out online") {
+    message = "<p>Still out of stock, we'll try again at 7am tomorrow.</p><br /><p><a href='https://isbootscurlcremeinstockyet.vercel.app/'>Check again any time here</a></p>"
+    subject = "Boots Curl Creme is still sold out."
+  } else {
+    message = "<p>Curl creme is no longer showing as out of stock on boots.com</p><p><a href='https://www.boots.com/boots-essentials-curl-creme-250ml-10088417'>Go check</a></p>"
+    subject = "Boots Curl Creme may be back in stock!"
+  }
+
+  if(message && subject) {
+    await SendEmail(message, subject).then(response =>
+      res.status(200).json({ response })
+    )
+  } else {
+    res.status(500).json({ "error": "error" })
+  }
 
 }
 
@@ -22,14 +37,14 @@ async function SendEmail(message, subject) {
     "content": [
       {
         "type": "text/html",
-        "value": `<p>${message}</p><br /><p><a href="">Check again any time here</a></p>`
+        "value": `${message}`
       }
     ],
     "subject": `${subject}`,
     "personalizations": [
       {
         "to": [{
-          "email": "andrflett@gmail.com",
+          "email": "cristinanlfonseca@gmail.com",
           "name": "Cris"
         }]
       }
