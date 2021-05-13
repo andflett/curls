7752a974525de75f2ee7e7675c8a283097cfe05f
Andrew Flett <andrflett@gmail.com>
2021-05-13 13:01:48 +0100

    WIP

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,50 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+export default async function SendQuestion(req, res) {
 
-  data = fetch('https://www.boots.com/webapp/wcs/stores/servlet/CVOSShowStockView?productId=927582&storeId=11352&storeId=11352&catalogId=28501&langId=-1&krypto=46Kqi7H%2B5ZzMDs8tfhrzBzmoY1PafMUQEJLnYsD17cvgmlVO8ZyCxkgPU3sMj3pbUhvMAGVmLrlJEjTMfBopJ8K4wPBwMvjTjyHt0IiclqkAQmRzITqlSYWhs7YL%2FmpFwfJfWwWYzfSaG2pygUpedO8XJCePq1ev9SVTT52cvbFjhHgTpk6a20IYhbKp2wZ7grog8glpTE4bEUJr8VSPfDHV3fy9w0C87wdjCYh%2FcOo%3D')
-    .then(response => console.log(response))
-    .then(body => console.log(body))
-    .catch(function(error) {
-      console.log(error);
-    });
+  console.log(req)
 
-  res.status(200).json({ data })
+  await SendEmail(req.body.message).then(response =>
+    res.status(200).json({ response })
+  )
+
+});
+
+async function SendEmail(message, subject) {
+
+  const authConfig = {
+     headers: {
+        Authorization: "Bearer " + process.env.SENDGRID_TOKEN
+     }
+  }
+
+  const email = {
+    "content": [
+      {
+        "type": "text/html",
+        "value": `<p>${message}</p><p><a href="">Check any time here</a></p>`
+      }
+    ],
+    "subject": `${subject}`,
+    "personalizations": [
+      {
+        "to": [{
+          "email": "cristinanlfonseca@gmail.com",
+          "name": "Cris"
+        }]
+      }
+    ],
+    "from": {
+      "email": 'andrflett@gmail.com'
+      "name": 'Andrew Flett'
+    }
+  }
+
+  axios.post('https://api.sendgrid.com/v3/mail/send', email, authConfig)
+  .then((res) => {
+    resolve(res.response?.data)
+  })
+  .catch((res) => {
+    reject(res.response?.data?.errors)
+  })
 
 }
--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
       <Head>
         <title>Is Boots Essentials Curl Creme In Stock Yet?</title>
         <meta name="description" content="Is Boots Essentials Curl Creme In Stock Yet?" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&family=Lato:wght@100;300;400;700;900&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&family=Lato:wght@100;300;400;700;900&display=swap" media="print" onload="this.media='all'" />
       </Head>
